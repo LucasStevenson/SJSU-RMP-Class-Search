ca472e8e5575885fce760b84677016a9905b3c0c
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2023-12-08 15:58:20 -0800

    data on the homepage is now fetched from the backend via http requests instead of being directly passed in

--- static/js/LiveSearch.js
+++ static/js/LiveSearch.js
@@ -1,4 +1,35 @@
-function addColorToTable() {
+const searchBar = document.querySelector(".text-input");
+let trie;
+let courseCode_to_classData = {};
+let teacherName_to_legacyID;
+
+(async () => {
+    // fetch all the class data from the backend
+    let classData = await fetch("/api/data/all_classes");
+    let all_classes = await classData.json();
+    // fetch the dict that maps teacher names to their legacyID
+    let data = await fetch("/api/data/teacherName_to_legacyID");
+    teacherName_to_legacyID = await data.json() // ex. { "Bob Smith": 123456, ... }
+
+    for (let classData of all_classes) {
+        courseCode_to_classData[classData[0].toLowerCase()] = classData;
+    }
+
+    all_classes = all_classes.map(x => x[0]); // only want to search via course codes. Ex. CS 152
+    trie = new Trie();
+    // insert data into the trie
+    all_classes.forEach(classTitle => trie.insert(classTitle)); 
+
+    // show page 1 data on initial page load
+    showPageData(1);
+
+    // Event listener for searchbar input change
+    searchBar.addEventListener("input", function (e) {
+        showPageData(1);
+    });
+})();
+
+function _addColorToTable() {
     let instruct_ratings = document.querySelectorAll(".instruct-rating");
     // logic to update the color of the instructor rating on the table depending on their rating
     for (let rating_tag of instruct_ratings) {
@@ -14,7 +45,7 @@ function addColorToTable() {
     }
 }
 
-function updateTableData(trie_results, courseCode_to_classData, teacherName_to_legacyID) {
+function _updateTableData(trie_results) {
     let tableBody = document.getElementById("table-body");
 
     // clear all the data that is currently inside the table body
@@ -54,52 +85,26 @@ function updateTableData(trie_results, courseCode_to_classData, teacherName_to_l
     }
 }
 
-addColorToTable();
-
-let courseCode_to_classData = {};
-for (let classData of all_classes) {
-    courseCode_to_classData[classData[0].toLowerCase()] = classData;
-}
-
-all_classes = all_classes.map(x => x[0]); // only want to search via course codes. Ex. CS 152
-const trie = new Trie();
-// insert data into the trie
-all_classes.forEach(classTitle => trie.insert(classTitle)); 
-
-const searchBar = document.querySelector(".text-input");
-
-// Event listener for keyup
-searchBar.addEventListener("input", function (e) {
-    setPageNumber(1);
-    updateTable();
-    let table_results = getMatchingClassesFromSearchbar();
-    // hide the pagination bar if there is less than 1 page of results, otherwise show it
-    document.querySelector(".pagination-container").style.display = table_results.length <= RESULTS_PER_PAGE ? "none" : "block";
-});
-
-function setPageNumber(pageNum) {
+function _setPageNumber(pageNum, table_results) {
     page = pageNum;
     document.querySelector(".active").innerText = page;
     // hide or show the `prev` button depending on if we're on first page or not
     document.querySelector(".prev").style.display = page === 1 ? "none" : "block";
     // hide or show the `next` button depending on if we're on the last page or not
-    let table_results = getMatchingClassesFromSearchbar();
     document.querySelector(".next").style.display = table_results.slice(page*RESULTS_PER_PAGE).length < 1 ? "none" : "block";
 }
 
-function getMatchingClassesFromSearchbar() {
+function _getMatchingClassesFromSearchbar() {
     let searchText = searchBar.value.trim().toLowerCase();
     let results = trie.search(searchText);
     return results;
 }
 
-function updateTable() {
-    let table_results = getMatchingClassesFromSearchbar();
-    updateTableData(table_results, courseCode_to_classData, teacherName_to_legacyID);
-    addColorToTable();
-}
-
 function showPageData(pageNum) {
-    setPageNumber(pageNum);
-    updateTable();
+    let table_results = _getMatchingClassesFromSearchbar();
+    _setPageNumber(pageNum, table_results);
+    _updateTableData(table_results);
+    _addColorToTable();
+    // hide the pagination bar if there is less than 1 page of results, otherwise show it
+    document.querySelector(".pagination-container").style.display = table_results.length <= RESULTS_PER_PAGE ? "none" : "block";
 }
